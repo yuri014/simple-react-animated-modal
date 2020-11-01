97422f7cb94d44059875d54d71c17813563148ab
Yuri <yuriflores2012@gmail.com>
2020-11-01 00:03:21 -0300

    adding props and styles

--- src/index.tsx
+++ src/index.tsx
@@ -5,22 +5,28 @@ interface AnimatedModalProps {
   show: boolean;
   onHide: () => void;
   title: string | React.ReactNode;
-  icon?: React.ReactNode;
+  closeButton?: React.ReactNode | string;
+  style?: React.CSSProperties | undefined;
+  headBorderBottom?: string;
 }
 
 export const AnimatedModal: React.FC<AnimatedModalProps> = (props) => {
+  const titleStyle = {
+    borderBottom: props.headBorderBottom,
+  };
+
   return (
     <div
       className={props.show ? styles.modalBlockTrue : styles.modalBlockFalse}
     >
-      <div className={styles.modalContent}>
-        <div className={styles.modalHead}>
-          <div className={styles.modalTitle}>
+      <div style={props.style} className={styles.modalContent}>
+        <div style={titleStyle} className={styles.modalHead}>
+          <span className={styles.modalTitle}>
             <h2>{props.title}</h2>
-          </div>
-          <div onClick={props.onHide} className={styles.closeModal}>
-            {props.icon ? props.icon : 'X'}
-          </div>
+          </span>
+          <span onClick={props.onHide} className={styles.closeModal}>
+            {props.closeButton ? props.closeButton : 'X'}
+          </span>
         </div>
         {props.children}
       </div>
