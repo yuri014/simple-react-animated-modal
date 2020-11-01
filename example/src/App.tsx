a235adb4b9e949cc30a832947e96078867e97797
Yuri <yuriflores2012@gmail.com>
2020-11-01 00:03:37 -0300

    passing props to component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 
-import { AnimatedModal } from 'simple-react-animated-modal'
-import 'simple-react-animated-modal/dist/index.css'
+import { AnimatedModal } from 'simple-react-animated-modal';
+import 'simple-react-animated-modal/dist/index.css';
 
 const App = () => {
-  const [showModal, setShowModal] = React.useState(false);
+  const [showModal, setShowModal] = React.useState(true);
 
   return (
     <div>
@@ -13,15 +13,21 @@ const App = () => {
         show={showModal}
         title="Example"
         onHide={() => setShowModal(false)}
+        closeButton={<strong>X</strong>}
+        style={{
+          backgroundColor: "#1e2939",
+          border: "2px solid #1cc5b7",
+          color: "white",
+          boxShadow: "2px 2px 4px black",
+        }}
+        headBorderBottom="1px solid white"
       >
         <div className="modal-body">
-          <p>
-            This is an exemple.
-          </p>
+          <p>This is an exemple.</p>
         </div>
       </AnimatedModal>
     </div>
   );
-}
+};
 
-export default App
+export default App;
