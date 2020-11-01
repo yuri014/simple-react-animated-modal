658553f36c3afaf23d8743a86857f068e51c17a8
Yuri <yuriflores2012@gmail.com>
2020-10-31 21:52:17 -0300

    create base component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
-import { ExampleComponent } from 'simple-react-animated-modal'
+import { AnimatedModal } from 'simple-react-animated-modal'
 import 'simple-react-animated-modal/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [showModal, setShowModal] = React.useState(false);
+
+  return (
+    <div>
+      <button onClick={() => setShowModal(true)}>Click Here</button>
+      <AnimatedModal
+        show={showModal}
+        title="Example"
+        onHide={() => setShowModal(false)}
+      >
+        <div className="modal-body">
+          <p>
+            This is an exemple.
+          </p>
+        </div>
+      </AnimatedModal>
+    </div>
+  );
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,29 @@
-import * as React from 'react'
-import styles from './styles.module.css'
+import * as React from 'react';
+import styles from './styles.module.css';
 
-interface Props {
-  text: string
+interface AnimatedModalProps {
+  show: boolean;
+  onHide: () => void;
+  title: string | React.ReactNode;
+  icon?: React.ReactNode;
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+export const AnimatedModal: React.FC<AnimatedModalProps> = (props) => {
+  return (
+    <div
+      className={props.show ? styles.modalBlockTrue : styles.modalBlockFalse}
+    >
+      <div className={styles.modalContent}>
+        <div className={styles.modalHead}>
+          <div className={styles.modalTitle}>
+            <h2>{props.title}</h2>
+          </div>
+          <div onClick={props.onHide} className={styles.closeModal}>
+            {props.icon ? props.icon : 'X'}
+          </div>
+        </div>
+        {props.children}
+      </div>
+    </div>
+  );
+};
